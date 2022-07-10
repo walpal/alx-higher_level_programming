763f37bb752d2bbdfac202be5d418f7c0eb41e6c
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-10 19:04:00 +0100

    Update 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,2 +1,8 @@
-#!/bin/bash
-su betty
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    else:
+        if c > b:
+            return a + b
+    return (a * b) - c
