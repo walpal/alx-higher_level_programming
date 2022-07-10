82ea921f8ec3be30e24068a9fafcb8714c60f749
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-10 22:16:39 +0100

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for number in range(100):
-    if (number != 99):
-        print("{}{}, ".format(int(number / 10), number % 10), end="")
-    else:
-        print("{}{}".format(int(number / 10), number % 10))
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
