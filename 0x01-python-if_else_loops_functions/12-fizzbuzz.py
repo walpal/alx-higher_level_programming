299522b6d8b73ce81caee92782f1b98f95878090
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-10 19:05:52 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,2 +1,10 @@
-#!/bin/bash
-su betty
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif num % 5 == 0:
+            print("Buzz ", end="")
+        elif num % 3 == 0:
+            print("Fizz ", end="")
+        else:
