cb36715c2391a5febd606923683a5dde6840a37f
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-10 19:26:03 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,7 @@
-#!/bin/bash
-su betty
+#!/usr/bin/python3
+for number in range(100):
+    if int(number / 10) != number % 10 and int(number / 10) < number % 10:
+        print("{}{}".format(int(number / 10), number % 10), end="")
+        if (number != 89):
+            print(", ", end="")
+print("")
