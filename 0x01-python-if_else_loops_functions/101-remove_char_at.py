097db5d18e25f94829ac79f159750c1c94af38d5
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-10 18:57:48 +0100

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,2 +1,9 @@
-#!/bin/bash
-su betty
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    i = 0
+    new_str = ""
+    for ch in str:
+        if i != n:
+            new_str += ch
+        i += 1
+    return 
