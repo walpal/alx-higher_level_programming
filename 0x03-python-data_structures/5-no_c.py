4eb7be677bff44dc9be3afe915ef949f78dd3deb
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 10:45:12 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-for number in range(100):
-    if (number != 99):
-        print("{}{}, ".format(int(number / 10), number % 10), end="")
-    else:
-        print("{}{}".format(int(number / 10), number % 10))
+def no_c(my_string):
+    length = len(my_string)
+
+    j = 0
+
+    new_string = my_string[:]
+
+    for i in range(length):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            new_string = new_string[:(i - j)] + my_string[(i + 1):]
+            j += 1
+
+    return (new_string)
