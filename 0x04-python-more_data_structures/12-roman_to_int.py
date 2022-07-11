d5107188e9ef765b60171aeda2986eb1f3dc87fc
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 09:40:45 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for num in range(1, 101):
-        if num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz ", end="")
-        elif num % 5 == 0:
-            print("Buzz ", end="")
-        elif num % 3 == 0:
-            print("Fizz ", end="")
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
         else:
-            print(f"{num} ", end="")
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
