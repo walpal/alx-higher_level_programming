be0a331b19a9352b7bf8f18fe4b8a23f565af481
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 10:40:41 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
