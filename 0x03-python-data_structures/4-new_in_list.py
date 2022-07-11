9bd44c5fb3c2d183776c956e8adfacf32717fbe3
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 10:44:44 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-for num in range(99):
-    print("{:d} = {:s}".format(num, hex(num)))
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+
+    new_list = my_list[:]
+
+    if 0 <= idx < length:
+        new_list[idx] = element
+
+    return (new_list)
