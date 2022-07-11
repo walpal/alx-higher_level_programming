76ce3bb14d2b8bd06749dc7718d79f53b8a46871
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 10:44:18 +0100

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for char in range(26):
-    if char != 4 and char != 16:
-        print("{:s}".format(chr(char + ord("a"))), end="")
+def print_reversed_list_integer(my_list=[]):
+
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
