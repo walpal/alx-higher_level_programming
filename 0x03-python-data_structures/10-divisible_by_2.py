e132e20429d27c9de74370161aee3aed8719df7e
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 10:41:09 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
+def divisible_by_2(my_list=[]):
+    check_div = []
 
-    return (max(a_dictionary, key=a_dictionary.get))
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            check_div.append(True)
+        else:
+            check_div.append(False)
+
+    return (check_div)
