085cfa3e5650ec5facb791fd23fd2e9184dde5c1
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 10:41:33 +0100

    Update 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,13 +1,27 @@
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
+#include <stdio.h>
+#include <Python.h>
 
-    num = 0
-    den = 0
+/**
+ * print_python_list_info - prints python list info
+ *
+ * @p: PyObject
+ * Return: no return
+ */
+void print_python_list_info(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *item;
 
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %ld\n", size);
 
-    return (num / den)
+	list = (PyListObject *)p;
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
