0674ac521e6ee0bc774589f54adb7fc621748266
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 10:38:45 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -24,6 +24,3 @@ if __name__ == "__main__":
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
