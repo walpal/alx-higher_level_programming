67923762b2c78d328733e80a0eee5d7163d2ac39
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-11 09:30:38 +0100

    Update 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    i = 0
-    new_str = ""
-    for ch in str:
-        if i != n:
-            new_str += ch
-        i += 1
-    return new_str
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
