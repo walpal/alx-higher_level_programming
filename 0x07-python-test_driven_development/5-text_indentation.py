16e555a2c76a397689829dd6674bd3be7d64d70c
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-27 17:47:46 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
-str = "Holberton School"
-print(3 * str)
-print(str[:9])
+"""
+Module text_indentation
+Adds two new lines after a set of characters.
+"""
+
+
+def text_indentation(text):
+    """Prints text with added two newlines
+    after each of these characters {'.', '?', ':'}.
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
