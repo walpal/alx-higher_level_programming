06ac2c8d267ce79947f9ec07e9eeefa92cad5df8
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2023-01-06 11:30:03 +0000

    classes_and_objects

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Author: ThankGod Ugobo
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
